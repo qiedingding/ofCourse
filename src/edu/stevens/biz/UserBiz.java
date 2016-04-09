087108c1048b52245a85aae3fba9d4cea1d52821
Xuan Li <xli100@stevens.edu>
2016-04-09 12:21:44 -0400

    Added files via upload

---
+++ src/edu/stevens/biz/UserBiz.java
@@ -0,0 +1,15 @@
+package edu.stevens.biz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stevens.entity.User;
+
+public interface UserBiz {
+
+	public ArrayList findAll();
+	public User findById(int id);
+	public boolean update(User user);
+	public boolean delete(User user);
+	public boolean validate_user(User user);
+}
