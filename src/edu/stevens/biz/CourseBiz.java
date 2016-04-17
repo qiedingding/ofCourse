052e1475919e9c2c87ea184e8b008bf47804fb96
Xuan Li <xli100@stevens.edu>
2016-04-17 17:08:59 -0400

    Added files via upload

--- src/edu/stevens/biz/CourseBiz.java
+++ src/edu/stevens/biz/CourseBiz.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import edu.stevens.entity.Course;
 import edu.stevens.entity.Custom;
+import edu.stevens.entity.User;
 
 public interface CourseBiz {
 	public ArrayList findAll();
@@ -13,6 +14,6 @@ public interface CourseBiz {
 	public boolean update(Course course);
 	public boolean delete(Course course);
 	public boolean insert(Course course);
-	public ArrayList recommend(Course course);
+	public ArrayList recommend(Course course  ,User user);
 	
 }
